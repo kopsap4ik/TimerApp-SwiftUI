25ab7683a89ec09dfdd5087ecb11b9f898bc1155
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-22 14:21:35 +0300

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Василий Петухов on 22.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
