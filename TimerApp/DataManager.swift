98af53f913f9afdbbb1b48a8acc8d7ba996a6265
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-28 14:00:51 +0300

    использован новый подход для работы с UserDefaults в DataManager

--- TimerApp/DataManager.swift
+++ TimerApp/DataManager.swift
@@ -5,23 +5,24 @@
 //  Created by Василий Петухов on 26.10.2020.
 //
 
-import Foundation
+// MARK:  - новый подход для работы с UserDefaults
+import SwiftUI
 
 final class DataManager {
     static let shared = DataManager()
     
-    private init (){}
+    // указывается ключ для UserDefaults
+    // используется Data(), так как в данном варианте работаем с данным типом
+    @AppStorage ("User") private var userData = Data()
     
-    private let userDefaults = UserDefaults.standard
-    private let keyUserDefaults = "User"
+    private init (){}
     
     func saveUser(user: User) {
         guard let userData = try? JSONEncoder().encode(user) else { return }
-        userDefaults.setValue(userData, forKey: keyUserDefaults)
+        self.userData = userData
     }
     
     func loadUser() -> User {
-        guard let userData = userDefaults.data(forKey: keyUserDefaults) as Data? else { return User() }
         guard let user = try? JSONDecoder().decode(User.self, from: userData) else { return User() }
         return user
     }
@@ -29,7 +30,40 @@ final class DataManager {
     func deleteUser(userManager: UserManager) {
         userManager.user.name = ""
         userManager.user.isRegistered = false
-        userDefaults.removeObject(forKey: keyUserDefaults)
+        UserDefaults.standard.removeObject(forKey: "User")
     }
     
 }
+
+// MARK:  - стандартный подход для работы с UserDefaults
+
+//import Foundation
+//
+//final class DataManager {
+//    static let shared = DataManager()
+//
+//
+//
+//    private let userDefaults = UserDefaults.standard
+//    private let keyUserDefaults = "User"
+//
+//    private init (){}
+//
+//    func saveUser(user: User) {
+//        guard let userData = try? JSONEncoder().encode(user) else { return }
+//        userDefaults.setValue(userData, forKey: keyUserDefaults)
+//    }
+//
+//    func loadUser() -> User {
+//        guard let userData = userDefaults.data(forKey: keyUserDefaults) as Data? else { return User() }
+//        guard let user = try? JSONDecoder().decode(User.self, from: userData) else { return User() }
+//        return user
+//    }
+//
+//    func deleteUser(userManager: UserManager) {
+//        userManager.user.name = ""
+//        userManager.user.isRegistered = false
+//        userDefaults.removeObject(forKey: keyUserDefaults)
+//    }
+//
+//}
